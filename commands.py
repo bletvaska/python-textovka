f4275d806074db5f5f031441eef11e8c9f8538cd
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-09-25 15:56:32 +0200

    refaktoring

--- commands.py
+++ commands.py
@@ -26,3 +26,100 @@ def show_inventory(backpack):
             print(f'     {item["name"]}')
     else:
         print('Batoh je prázdny.')
+
+
+def about():
+    print(
+        'Ta tuto mocnu hru o Indianovi Jonesovi spachal uz nie az taky mlady fajny programator mirek v roku 2019. A ak ho chces podporit, ta mozes nejake fsimne poslat na ucet s IBANom SK1234567890987654321')
+
+
+def help():
+    print('ta pomoz si sam.')
+
+
+def commands():
+    print('rozhliadni sa\no hre\npomoc\nprikazy')
+
+
+def quit():
+    print('ta diky ze si si zahral tuto mocnu hru, lebo je fakt mocna.')
+
+
+def east(world, current_room):
+    """
+    Enter the room on east from current room.
+    If there is no exit to the east, then no change. The new room will be returned from the function.
+    :param world: the world the player is in
+    :param current_room: the name of current room player is in
+    :return: the name of new room on the east
+    """
+    room = world[current_room]
+
+    if 'vychod' in room['exits']:
+        current_room = room['exits']['vychod']
+        show_room(world[current_room])
+    else:
+        print('tam sa neda ist')
+
+    return current_room
+
+
+def west(world, current_room):
+    room = world[current_room]
+
+    if 'zapad' in room['exits']:
+        current_room = room['exits']['zapad']
+        show_room(world[current_room])
+    else:
+        print('tam sa neda ist')
+
+    return current_room
+
+
+def north(world, current_room):
+    room = world[current_room]
+
+    if 'sever' in room['exits']:
+        current_room = room['exits']['sever']
+        show_room(world[current_room])
+    else:
+        print('tam sa neda ist')
+
+    return current_room
+
+
+def south(world, current_room):
+    room = world[current_room]
+
+    if 'juh' in room['exits']:
+        current_room = room['exits']['juh']
+        show_room(world[current_room])
+    else:
+        print('tam sa neda ist')
+
+    return current_room
+
+
+def down(world, current_room):
+    room = world[current_room]
+
+    if 'dolu' in room['exits']:
+        current_room = room['exits']['dolu']
+        show_room(world[current_room])
+    else:
+        print('tam sa neda ist')
+
+    return current_room
+
+
+def drop_item(world, current_room, backpack, name):
+    room = world[current_room]
+    for item in backpack:
+        if item['name'] == name:
+            room['items'].append(item)
+            backpack.remove(item)
+            print(f'{item["name"]} si vyložil z batohu.')
+            break
+    else:
+        print('Taký predmet u seba nemáš.')
+
--- game.py
+++ game.py
@@ -3,100 +3,63 @@ from world import world
 from commands import *
 
 
-current_room = 'pred jaskynou'
-backpack = [
-    {
-        'name': 'bic',
-        'description': 'Ta mocny bic na krotenie levov, ktory si si zohnal ako mlady chalanisko v tretej casti serie.'
-    }
-]
+def main():
+    current_room = 'pred jaskynou'
+    backpack = [
+        {
+            'name': 'bic',
+            'description': 'Ta mocny bic na krotenie levov, ktory si si zohnal ako mlady chalanisko v tretej casti serie.'
+        }
+    ]
 
-print("Indiana Jones")
-show_room(world[current_room])
+    print("Indiana Jones")
+    show_room(world[current_room])
 
-answer = None
+    answer = None
 
-while answer != 'koniec':
-    answer = input('> ').strip().lower()
+    while answer != 'koniec':
+        answer = input('> ').strip().lower()
 
-    if answer == 'rozhliadni sa':
-        show_room(world[current_room])
-
-    elif answer == 'o hre':
-        print(
-            'Ta tuto mocnu hru o Indianovi Jonesovi spachal uz nie az taky mlady fajny programator mirek v roku 2019. A ak ho chces podporit, ta mozes nejake fsimne poslat na ucet s IBANom SK1234567890987654321')
-
-    elif answer == 'pomoc':
-        print('ta pomoz si sam.')
-
-    elif answer == 'prikazy':
-        print('rozhliadni sa\no hre\npomoc\nprikazy')
-
-    elif answer == 'koniec':
-        print('ta diky ze si si zahral tuto mocnu hru, lebo je fakt mocna.')
-
-    elif answer == 'vychod':
-        room = world[current_room]
-
-        if 'vychod' in room['exits']:
-            current_room = room['exits']['vychod']
+        if answer == 'rozhliadni sa':
             show_room(world[current_room])
+
+        elif answer == 'o hre':
+            about()
+
+        elif answer == 'pomoc':
+            help()
+
+        elif answer == 'prikazy':
+            commands()
+
+        elif answer == 'koniec':
+            quit()
+
+        elif answer == 'vychod':
+            current_room = east(world, current_room)
+
+        elif answer == 'zapad':
+            current_room = west(world, current_room)
+
+        elif answer == 'sever':
+            current_room = north(world, current_room)
+
+        elif answer == 'juh':
+            current_room = south(world, current_room)
+
+        elif answer == 'dolu':
+            current_room = down(world, current_room)
+
+        elif answer == 'inventar':
+            show_inventory(backpack)
+
+        elif answer.startswith('poloz'):
+            name = answer.strip('poloz').strip()
+            drop_item(world, current_room, backpack, name)
+
         else:
-            print('tam sa neda ist')
-
-    elif answer == 'zapad':
-        room = world[current_room]
-
-        if 'zapad' in room['exits']:
-            current_room = room['exits']['zapad']
-            show_room(world[current_room])
-        else:
-            print('tam sa neda ist')
-
-    elif answer == 'sever':
-        room = world[current_room]
-
-        if 'sever' in room['exits']:
-            current_room = room['exits']['sever']
-            show_room(world[current_room])
-        else:
-            print('tam sa neda ist')
-
-    elif answer == 'juh':
-        room = world[current_room]
-
-        if 'juh' in room['exits']:
-            current_room = room['exits']['juh']
-            show_room(world[current_room])
-        else:
-            print('tam sa neda ist')
-
-    elif answer == 'dolu':
-        room = world[current_room]
-
-        if 'dolu' in room['exits']:
-            current_room = room['exits']['dolu']
-            show_room(world[current_room])
-        else:
-            print('tam sa neda ist')
-
-    elif answer == 'inventar':
-        show_inventory(backpack)
-
-    elif answer.startswith('poloz'):
-        room = world[current_room]
-        name = answer.strip('poloz').strip()
-        for item in backpack:
-            if item['name'] == name:
-                room['items'].append(item)
-                backpack.remove(item)
-                print(f'{item["name"]} si vyložil z batohu.')
-                break
-        else:
-            print('Taký predmet u seba nemáš.')
+            print('ta taky prikaz nepoznam')
 
 
-
-
-    else:
-        print('ta taky prikaz nepoznam')
+if __name__ == '__main__':
+    main()
