0504ecd95e5c3c3eaba1d6cd6e0e89596b23e91d
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-09-25 11:29:04 +0200

    roztrhnutie na world a game

---
+++ world.py
@@ -0,0 +1,17 @@
+world = {
+    'pred jaskynou': {
+        'name': 'pred jaskynou',
+        'description': "Stojis pred vchodom do tajomnej jaskyne. Jaskyna nevesti nic dobre.",
+        'exits': {
+            'vychod': 'v jaskyni'
+        }
+    },
+
+    'v jaskyni': {
+        'name': 'v jaskyni',
+        'description': 'Stojis v temnej jaskyni. Uz aj tak mizernu viditelnost znizuju este pavuciny, ktorych je tu teda riadna kopa.',
+        'exits': {
+            'zapad': 'pred jaskynou'
+        }
+    }
+}
