55f74c65eff50c3e8a84673d48e9f5086872e366
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-09-25 13:41:50 +0200

    opravene zobrazenie miestnosti

--- game.py
+++ game.py
@@ -4,7 +4,12 @@ from world import world
 
 def show_room(room):
     print(room['description'])
-    print('Mozes ist na ', room['exits'])
+    if len(room['exits']) > 0:
+        print('Možné východy:')
+        for exit in room['exits']:
+            print(f"    {exit}")
+    else:
+        print('Z miestnosti nevedú žiadne východy.')
 
 
 current_room = 'pred jaskynou'
