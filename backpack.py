43c6eed6fb41d289fb8264cc1bd346e1748942ae
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-12-13 15:46:34 +0100

    posledna verzia

--- backpack.py
+++ backpack.py
@@ -6,27 +6,6 @@ class Backpack:
         self._capacity = capacity
         self._items = []
 
-    def append(self, item):
-        if len(self._items) >= self._capacity:
-            print('Batoh je plný.')
-            return
-
-        self._items.append(item)
-
-    def get(self, name):
-        for item in self._items:
-            if item._name == name:
-                return item
-        else:
-            return None
-
-    def remove(self, item: Item):
-        self._items.remove(item)
-
-    def list(self):
-        for item in self._items:
-            print(item._name)
-
     def __len__(self):
         return len(self._items)
 
