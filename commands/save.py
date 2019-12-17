0c49d9b35bd5e2db3eec180aaaf516c56d2c4e04
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-12-17 13:50:56 +0100

    ulozenie historie do suboru hotove. hirosima hadr.

--- commands/save.py
+++ commands/save.py
@@ -12,9 +12,23 @@ class Save(Command):
             return
 
         # uloz subor riadok po riadku
-        with open(self._params, 'w') as f:
-            for cmd in context.history:
-                f.write(f'{cmd}\n')
+        try:
+            with open(self._params, 'w') as f:
+                for cmd in context.history:
+                    f.write(f'{cmd}\n')
 
-        # vypis spravu pre pouzivatela
-        print('Hra bola úspešne uložená.')
+            # vypis spravu pre pouzivatela
+            print('Hra bola úspešne uložená.')
+
+        except FileNotFoundError as ex:
+            print(f'Chyba: cesta "{self._params}" neexistuje.')
+
+        except PermissionError as ex:
+            print(f'Chyba: nemáš práva na zápis súboru "{self._params}".')
+
+        except IsADirectoryError as ex:
+            print(f'Chyba: súbor "{self._params}" je priečinok. Si nečakal, čo?')
+
+        except Exception as ex:
+            print(f'Chyba: dajaká chyba.')
+            print(ex)
