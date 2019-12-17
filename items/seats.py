03cb666dac7ccb9348acfd2ae1c7253b42d9a050
Miroslav Biňas <miroslav.binas@tuke.sk>
2019-12-17 10:36:04 +0100

    mikroscenar pre preskumanie sedacky

--- items/seats.py
+++ items/seats.py
@@ -6,4 +6,9 @@ class Seats(Item):
         super().__init__('sedacky', 'Mäkké, jemné, spružiny trčiace sedačky lietadla. Ako nové. A len pre pilota.', ['examinable'])
 
     def examine(self, context):
-        print('ta preskumavam sedacky. podrobne.')
+        # 1. do miestnosti vlozim klucik
+
+        # 2. zrusim ficuru 'examinable'
+        
+        # 3. mocny text
+        print('Neodolal si a svoje veľké dlane si strčil medzi operadlo a sedadlo. Oči ti zažiarili, keď si nahmatal niečo chladné a kovové. V tvojej dlani sa objavil kľúčik. Značky FAB. Nič moc.')
