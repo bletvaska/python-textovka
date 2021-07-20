91e889c7841733ad444f792cfc7c53622610ef1f
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-07-20 13:12:05 +0200

    vytvoreny slovnik pre miestnost

--- game.py
+++ game.py
@@ -8,8 +8,14 @@ print('|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/')
 
 print('                                                     (c) 2021 mirek')
 
+room = {
+    'description': 'Nachádzaš v tmavej miestnosti. Kamenné múry dávajú tušiť, že sa nachádzaš v nejakej kamennej kobke. Žeby podzemie hradu v Grunwalde? Okná tu nie sú žiadne, čo by ťa uistili o správnosti tohto predpokladu.',
+    'name': 'kobka',
+    'items': None,
+    'exits': None
+}
 
-print('Nachádzaš v tmavej miestnosti. Kamenné múry dávajú tušiť, že sa nachádzaš v nejakej kamennej kobke. Žeby podzemie hradu v Grunwalde? Okná tu nie sú žiadne, čo by ťa uistili o správnosti tohto predpokladu.')
+print(room['description'])
 
 line = None
 
@@ -19,6 +25,9 @@ while line != 'koniec':
     if line == 'o hre':
         print('Hru spáchal  (c)2021 mirek')
         print('Ďalší príbeh Indiana Jonesa sa odohráva v temnej komôrke.')
+
+    elif line == 'rozhliadni sa':
+        print(room['description'])
 
     elif line == 'prikazy':
         print('Dostupné príkazy v hre:')
