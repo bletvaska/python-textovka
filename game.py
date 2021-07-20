2bda301ba2547f01cd913bf8af1d8dfb009e2339
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-07-20 12:50:49 +0200

    opis miestnosti

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ print('|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/')
 print('                                                     (c) 2021 mirek')
 
 
+print('Nachádzaš v tmavej miestnosti. Kamenné múry dávajú tušiť, že sa nachádzaš v nejakej kamennej kobke. Žeby podzemie hradu v Grunwalde? Okná tu nie sú žiadne, čo by ťa uistili o správnosti tohto predpokladu.')
+
 line = None
 
 while line != 'koniec':
