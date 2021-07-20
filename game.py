42c7cf482847cce5c789a2dc5a912c4034cd75bd
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-07-20 13:30:37 +0200

    vytvorena funkcia show_room(), refaktoring s __name__

--- game.py
+++ game.py
@@ -1,42 +1,50 @@
 #!/usr/bin/env python3
 
-print(' ___           _ _                         _                       ')
-print('|_ _|_ __   __| (_) __ _ _ __   __ _      | | ___  _ __   ___  ___ ')
-print(" | || '_ \ / _` | |/ _` | '_ \ / _` |  _  | |/ _ \| '_ \ / _ \/ __|")
-print(" | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\__ \\")
-print('|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/')
+def show_room(room):
+    print(f'Nachádzaš sa v miestnosti {room["name"]}')
+    print(room['description'])
+    print()
 
-print('                                                     (c) 2021 mirek')
+if __name__ == '__main__':
+    print(' ___           _ _                         _                       ')
+    print('|_ _|_ __   __| (_) __ _ _ __   __ _      | | ___  _ __   ___  ___ ')
+    print(" | || '_ \ / _` | |/ _` | '_ \ / _` |  _  | |/ _ \| '_ \ / _ \/ __|")
+    print(" | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\__ \\")
+    print('|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/')
 
-room = {
-    'description': 'Nachádzaš v tmavej miestnosti. Kamenné múry dávajú tušiť, že sa nachádzaš v nejakej kamennej kobke. Žeby podzemie hradu v Grunwalde? Okná tu nie sú žiadne, čo by ťa uistili o správnosti tohto predpokladu.',
-    'name': 'kobka',
-    'items': None,
-    'exits': None
-}
+    print('                                                     (c) 2021 mirek')
 
-print(room['description'])
+    room = {
+        'description': 'Nachádzaš v tmavej miestnosti. Kamenné múry dávajú tušiť, že sa nachádzaš v nejakej kamennej kobke. Žeby podzemie hradu v Grunwalde? Okná tu nie sú žiadne, čo by ťa uistili o správnosti tohto predpokladu.',
+        'name': 'kobka',
+        'items': None,
+        'exits': None
+    }
 
-line = None
+    show_room(room)
 
-while line != 'koniec':
-    line = input('> ').rstrip().lstrip().lower()
+    line = None
 
-    if line == 'o hre':
-        print('Hru spáchal  (c)2021 mirek')
-        print('Ďalší príbeh Indiana Jonesa sa odohráva v temnej komôrke.')
+    while line != 'koniec':
+        line = input('> ').rstrip().lstrip().lower()
 
-    elif line == 'rozhliadni sa':
-        print(room['description'])
+        if line == 'o hre':
+            print('Hru spáchal  (c)2021 mirek')
+            print('Ďalší príbeh Indiana Jonesa sa odohráva v temnej komôrke.')
+            print()
 
-    elif line == 'prikazy':
-        print('Dostupné príkazy v hre:')
-        print('* koniec - ukončí rozohratú hru')
-        print('* o hre - zobrazí informácie o fantastickom autorovi hry a o hre samotnej')
-        print('* prikazy - zobrazí zoznam príkazov, ktoré hra podporuje')
+        elif line == 'rozhliadni sa':
+            show_room(room)
 
-    elif line in ('koniec', ''):
-        continue
+        elif line == 'prikazy':
+            print('Dostupné príkazy v hre:')
+            print('* koniec - ukončí rozohratú hru')
+            print('* o hre - zobrazí informácie o fantastickom autorovi hry a o hre samotnej')
+            print('* prikazy - zobrazí zoznam príkazov, ktoré hra podporuje')
+            print()
 
-    else:
-        print('Taký príkaz nepoznám.')
+        elif line in ('koniec', ''):
+            continue
+
+        else:
+            print('Taký príkaz nepoznám.')
