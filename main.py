86ec002890f7edd25aa494c2ca59cfd5ebe53c27
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-09-07 12:33:09 +0200

    vyriesene ukoncenie hry pomocou stavu

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+STATE_QUIT = 0
+STATE_PLAYING = 1
+
 print('Nachádzaš sa v tmavej miestnosti. Každé okno je zvonku zabarikádované a do miestnosti preniká len úzky prameň '
       'svetla. Masívne drevené dvere sú jediným východom z miestnosti.')
 
 line = None
+state = STATE_PLAYING
 
-while line != 'koniec':
+while state == STATE_PLAYING:
     line = input('> ').strip().lower()
 
     if line == 'o hre':
@@ -20,8 +24,9 @@ while line != 'koniec':
         print('* prikazy - zobrazí zoznam príkazov dostupných v hre')
         print('* rozhliadni sa - zobrazí obsah miestnosti')
 
-    elif line == 'koniec':  # quit, bye, q
+    elif line in ('koniec', 'quit', 'bye', 'q', 'ukoncit'):
         print('ta koncime')
+        state = STATE_QUIT
 
     else:
         print('Taký príkaz nepoznám.')
