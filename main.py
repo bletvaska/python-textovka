0f0058b8779101b15b527880d4ff2ee282d70177
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-09-07 12:39:03 +0200

    pridany prikaz rozhliadni sa

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 STATE_QUIT = 0
 STATE_PLAYING = 1
 
-print('Nachádzaš sa v tmavej miestnosti. Každé okno je zvonku zabarikádované a do miestnosti preniká len úzky prameň '
-      'svetla. Masívne drevené dvere sú jediným východom z miestnosti.')
+room = 'Nachádzaš sa v tmavej miestnosti. Každé okno je zvonku zabarikádované a do miestnosti preniká len úzky prameň ' \
+       'svetla. Masívne drevené dvere sú jediným východom z miestnosti. '
 
 line = None
 state = STATE_PLAYING
+
+print(room)
 
 while state == STATE_PLAYING:
     line = input('> ').strip().lower()
@@ -27,6 +29,9 @@ while state == STATE_PLAYING:
     elif line in ('koniec', 'quit', 'bye', 'q', 'ukoncit'):
         print('ta koncime')
         state = STATE_QUIT
+
+    elif line == 'rozhliadni sa':
+        print(room)
 
     else:
         print('Taký príkaz nepoznám.')
