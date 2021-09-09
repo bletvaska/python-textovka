f87e01514bae20a88593114a8eb1145339920653
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-09-09 14:56:09 +0200

    pouzitie ucebnice

--- commands.py
+++ commands.py
@@ -1,5 +1,29 @@
+import random
+
 import features
 import states
+
+_zen_of_python = [
+    'Beautiful is better than ugly.',
+    'Explicit is better than implicit.',
+    'Simple is better than complex.',
+    'Complex is better than complicated.',
+    'Flat is better than nested.',
+    'Sparse is better than dense.',
+    'Readability counts.',
+    "Special cases aren't special enough to break the rules.",
+    'Although practicality beats purity.',
+    'Errors should never pass silently.',
+    'Unless explicitly silenced.',
+    'In the face of ambiguity, refuse the temptation to guess.',
+    'There should be one-- and preferably only one --obvious way to do it.',
+    "Although that way may not be obvious at first unless you're Dutch.",
+    'Now is better than never.',
+    'Although never is often better than *right* now.',
+    "If the implementation is hard to explain, it's a bad idea.",
+    'If the implementation is easy to explain, it may be a good idea.',
+    "Namespaces are one honking great idea -- let's do more of those!",
+]
 
 
 def inventory(name: str, context: dict) -> None:
@@ -122,10 +146,18 @@ def use(name: str, context: dict) -> None:
             if item['name'] == name:
                 if features.USABLE not in item['features']:
                     print('Tento predmet sa nedá použiť.')
-                    return
                 else:
-                    print(f'Snažím sa použiť predmet {name}')
-                    return
+
+                    if name == 'ucebnica jazyka python':
+                        print('Zalistoval si v učebnici a dočítal si sa, že:')
+                        print(random.choice(_zen_of_python))
+
+                    elif name == 'kanister':
+                        print('ta som odsroboval a trosku si snupol. Fajnovy. vysoko-oktanovy.')
+
+                    else:
+                        print(f'Snažím sa použiť predmet {name}')
+                return
         else:
             print('Taký predmet tu nikde nevidím.')
 
