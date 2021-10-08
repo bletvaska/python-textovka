f133369016097334bae1f0501f3408ae8d7e26e7
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-10-08 13:33:52 +0200

    prikazy su v samostatnom module

---
+++ commands.py
@@ -0,0 +1,126 @@
+from features import MOVABLE, USABLE
+
+
+def cmd_take(room: dict, line: str, backpack: list):
+    item_name = line.removeprefix('vezmi').strip()
+
+    # is there name given?
+    if item_name == '':
+        print('Neviem čo chceš zobrať.')
+        return
+
+    # if item not in backpack
+    for item in room['items']:
+        if item['name'] == item_name:
+            # is item movable?
+            if MOVABLE in item['features']:
+                # is backpack full?
+                if len(backpack) >= 2:
+                    print('Batoh je plný.')
+                    return
+
+                # remove item from room
+                room['items'].remove(item)
+
+                # drop item in the backpack
+                backpack.append(item)
+
+                # print out
+                print(f'Predmet {item["name"]} si si vložil do batohu.')
+
+            else:
+                print('Tento predmet sa nedá zobrať.')
+
+            return
+
+    # if no such item available
+    print('Taký predmet tu nikde nevidím.')
+
+
+def cmd_inventory(backpack: list):
+    if backpack == []:
+        print('Batoh je prázdny.')
+    else:
+        print("V batohu máš:")
+        for item in backpack:
+            print(f'\t* {item["name"]}')
+
+
+def cmd_explore(room: dict, line: str, backpack: list):
+    item_name = line.removeprefix('preskumaj').strip()
+
+    # is there name given?
+    if item_name == '':
+        print('Neviem čo chceš preskúmať.')
+        return
+
+    # if item not in room items
+    for item in room['items'] + backpack:
+        if item['name'] == item_name:
+            print(item['description'])
+            return
+
+    # if no such item available
+    print('Taký predmet tu nikde nevidím.')
+
+
+def cmd_drop(room: dict, line: str, backpack: list):
+    item_name = line.removeprefix('poloz').strip()
+
+    # is there name given?
+    if item_name == '':
+        print('Neviem čo chceš položiť.')
+        return
+
+    # if item not in backpack
+    for item in backpack:
+        if item['name'] == item_name:
+            # remove item from backpack
+            backpack.remove(item)
+
+            # drop item in the room
+            room['items'].append(item)
+
+            # print out
+            print(f'Predmet {item["name"]} si položil do miestnosti.')
+            return
+
+    # if no such item available
+    print('Taký predmet u seba nemáš.')
+
+
+def cmd_look_around(room: dict):
+    """
+    Prints description about the room
+
+    Prints out the description about the room given as parameter.
+    :param room: room to describe
+    """
+    print(f'Nachádzaš sa v miestnosti {room["name"]}')
+    print(room['description'])
+
+    # print items in the room
+    if room['items'] == []:  # len(room['items'] == 0
+        print('Nevidíš tu nič zvláštne.')
+    else:
+        print("Vidíš:")
+        for item in room['items']:
+            print(f'\t* {item["name"]}')
+
+
+def cmd_commands():
+    print('Dostupné príkazy v hre:')
+    print('* inventar - zobrazí obsah hráčovho batoha')
+    print('* koniec - ukončí hru')
+    print('* o hre - zobrazí informácie o hre')
+    print('* poloz - vyloží predmet z batohu do miestnosti')
+    print('* prikazy - zobrazí zoznam dostupných príkazov v hre')
+    print('* rozhliadni sa - zobrazí opis miestnosti, v ktorej sa hráč aktuálne nachádza')
+    print('* vezmi - vezme predmet z miestnosti a vloží ho do batohu')
+
+
+def cmd_about():
+    print('Indiana Jones a jeho Pythoňácke dobrodružstvo')
+    print('Nestarnúci hrdina Indiana Jones sa tentokrát ocitol sám pustý v škaredej miestnosti. A jedine '
+          'Pythoňácky programátori mu môžu zachrániť krk. Je to na tebe.')
+    print('\n(c) 2021 hru spáchal mirek')
