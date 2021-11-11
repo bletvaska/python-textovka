f05181c06a9bbf92089b12f26fc9a3fe87b1bd6d
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-11-11 11:20:03 +0100

    added some comments

--- game.py
+++ game.py
@@ -17,23 +17,29 @@ if __name__ == '__main__':
         # normalizing string
         line = input('> ').lower().strip()
 
+        # empty input
         if line == '':
             continue
 
+        # quit game
         elif line in ('koniec', 'quit', 'bye', 'q'):
             game_state = states.QUIT
 
+        # about game
         elif line in ('o hre', 'about', 'info', '?'):
             print('(c)2021 created by mirek')
-            print('Ďalšie veľké dobrodružstvo Indiana Jonesa. Tentokrá zápasí s jazykom Python v tmavej miestnosti.')
+            print('Ďalšie veľké dobrodružstvo Indiana Jonesa. Tentokrát zápasí s jazykom Python v tmavej miestnosti.')
 
+        # show commands
         elif line in ('prikazy', 'commands', 'help', 'pomoc'):
             print('Zoznam príkazov v hre:')
             print('* koniec - ukončí rozohratú hru')
             print('* o hre - zobrazí informácie o hre')
             print('* prikazy - zobrazí príkazy, ktoré sa dajú použiť v hre')
 
+        # unknown commands
         else:
             print('Taký príkaz nepoznám.')
 
+    # game credits
     print('(c)2021 by mirek mocný programátor')
