6603042fdbc002c0ff09f44271727a050e0937fc
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-11-18 14:31:41 +0100

    pouzitie kanistra

--- items/canister.py
+++ items/canister.py
@@ -1,8 +1,28 @@
+from helpers import get_item_by_name
+from .door import SOAKED
 from .features import USABLE, MOVABLE
 
 
 def _use(context: dict) -> None:
-    raise NotImplementedError('Usage of this item was not yet implemented')
+    # check if door are in the room
+    door = get_item_by_name('dvere', context['room']['items'])
+    if door is None:
+        print('Vzal si kanister do ruky, trošku si zaposiloval a uľavil si si sprostým slovom. Hneď sa cítiš lepšie.')
+        return
+
+    # make door soaked door
+    door['description'] = 'Veľké masívne dubové dvere. Len tak niečo a niekto s nimi nepohne, keď sú zamknuté. A to ' \
+                          'teda sú. A ešte k tomu aj parádne nasiaknuté vysokooktánovým benzínom.'
+    door['state'] = SOAKED
+
+    # update canister
+    canister['description'] = 'Veľký 25L kanister. Odšroboval si veko a nadýchol si sa. Ešte pred chvíľou tu bol ' \
+                              'určite vysokooktánový benzín. Ale teraz tu už nie je nič.'
+    canister['features'].remove(USABLE)
+
+    # render
+    print('Odšroboval si vrchnák z kanistra, rozohnal si sa a celý jeho vysokooktánový obsah si vylial '
+          'na dubové dvere.')
 
 
 canister = {
--- items/door.py
+++ items/door.py
@@ -1,5 +1,10 @@
+SOAKED = 'soaked'
+NORMAL = 'normal'
+
 door = {
     'name': 'dvere',
     'description': 'Veľké masívne dubové dvere. Len tak niečo a niekto s nimi nepohne, keď sú zamknuté. A to teda sú.',
-    'features': []
+    'features': [],
+    'state': NORMAL
+
 }
