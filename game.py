48e00fa9c4d195710b4bf25a5149641000f3e70f
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-01-24 13:30:02 +0100

    herna slucka

--- game.py
+++ game.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
+
 print('Indiana Jones and his Great Escape')
 
-line = input('> ').lower().lstrip().rstrip()
+line = None
 
-if line == 'o hre':
-    print('Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou.')
-    print('Túto hru spáchal v 2022 (c) mirek.')
+while line != 'koniec':
 
-elif line == 'prikazy':
-    print('Zoznam dostupných príkazov:')
-    print('* o hre - zobrazí informácie o hre')
-    print('* prikazy - zobrazí zoznam príkazov hry')
+    line = input('> ').lower().lstrip().rstrip()
 
-else:
-    print('Taký príkaz nepoznám.')
+    if line == 'o hre':
+        print('Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou.')
+        print('Túto hru spáchal v 2022 (c) mirek.')
+
+    elif line == 'prikazy':
+        print('Zoznam dostupných príkazov:')
+        print('* koniec - ukončí rozohratú hru')
+        print('* o hre - zobrazí informácie o hre')
+        print('* prikazy - zobrazí zoznam príkazov hry')
+
+    elif line == 'koniec':
+        pass
+
+    else:
+        print('Taký príkaz nepoznám.')
 
 print('Created by (c)2022 mirek')
