ef9d8b6c1be011e97362c5e537232fe1b56852b2
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-01-24 15:16:14 +0100

    pridany prikaz rozhliadni sa

--- game.py
+++ game.py
@@ -2,56 +2,68 @@
 
 import states
 
-print(__name__)
-
 
 def play_game():
-    print(' ___           _ _                         _                       ')
-    print('|_ _|_ __   __| (_) __ _ _ __   __ _      | | ___  _ __   ___  ___ ')
+    print(" ___           _ _                         _                       ")
+    print("|_ _|_ __   __| (_) __ _ _ __   __ _      | | ___  _ __   ___  ___ ")
     print(" | || '_ \ / _` | |/ _` | '_ \ / _` |  _  | |/ _ \| '_ \ / _ \/ __|")
-    print(' | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\__ \\')
-    print('|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/')
-    print('             Indiana Jones and his Great Escape')
+    print(" | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\__ \\")
+    print("|___|_| |_|\__,_|_|\__,_|_| |_|\__,_|  \___/ \___/|_| |_|\___||___/")
+    print("             Indiana Jones and his Great Escape")
+    print()
 
-    print('Nachádzaš sa v tmavej miestnosti, v ktorej rozhodne chýbajú okná. '
-          'Je tu značne šero a vlhko. Chladné kamenné steny dávajú tušiť, že '
-          'sa nachádzaš v podzemí.')
+    print(
+        "Nachádzaš sa v tmavej miestnosti, v ktorej rozhodne chýbajú okná. "
+        "Je tu značne šero a vlhko. Chladné kamenné steny dávajú tušiť, že "
+        "sa nachádzaš v podzemí."
+    )
 
     game_state = states.PLAYING
 
     while game_state == states.PLAYING:
 
         # normalizing input string
-        line = input('> ').lower().lstrip().rstrip()
+        line = input("> ").lower().lstrip().rstrip()
 
         # empty input?
-        if line == '':
+        if line == "":
             continue
 
         # about game
-        elif line in ('o hre', 'about', 'info'):
-            print('Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou.')
-            print('Túto hru spáchal v 2022 (c) mirek.')
+        elif line in ("o hre", "about", "info"):
+            print(
+                "Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou."
+            )
+            print("Túto hru spáchal v 2022 (c) mirek.")
 
         # list of commands
-        elif line in ('prikazy', 'commands', 'help', '?'):
-            print('Zoznam dostupných príkazov:')
-            print(' * koniec - ukončí rozohratú hru')
-            print(' * o hre - zobrazí informácie o hre')
-            print(' * prikazy - zobrazí zoznam príkazov hry')
+        elif line in ("prikazy", "commands", "help", "?"):
+            print("Zoznam dostupných príkazov:")
+            print("  * koniec - ukončí rozohratú hru")
+            print("  * o hre - zobrazí informácie o hre")
+            print("  * prikazy - zobrazí zoznam príkazov hry")
+            print("  * rozhliadni sa - Vypise popis miestnosti, kde sa prave nachadzas.")
+
+        # look around
+        elif line in ("rozhliadni sa", "look around"):
+            print(
+                "Nachádzaš sa v tmavej miestnosti, v ktorej rozhodne chýbajú okná. "
+                "Je tu značne šero a vlhko. Chladné kamenné steny dávajú tušiť, že "
+                "sa nachádzaš v podzemí."
+            )
 
         # quit game
-        elif line in ('koniec', 'quit', 'q', 'bye'):
-            line = input('Naozaj chceš skončiť? (a/n) ').lower().lstrip().rstrip()
-            if line == 'a':
+        elif line in ("koniec", "quit", "q", "bye"):
+            line = input("Naozaj chceš skončiť? (a/n) ").lower().lstrip().rstrip()
+            if line == "a":
                 game_state = states.QUIT
 
         # unknown command
         else:
-            print('Taký príkaz nepoznám.')
+            print("Taký príkaz nepoznám.")
 
-    print('Created by (c)2022 mirek')
+    print("Created by (c)2022 mirek")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     play_game()
