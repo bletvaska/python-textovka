c5c54631c96d3cb738f53aa21f6c228af2ba780d
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-01-24 14:17:55 +0100

    komentare a osetrenie prazdneho vstupu

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# from states import 
 import states
 
 
@@ -10,23 +9,32 @@ game_state = states.PLAYING
 
 while game_state == states.PLAYING:
 
+    # normalizing input string
     line = input('> ').lower().lstrip().rstrip()
 
-    if line in ('o hre', 'about', 'info'):
+    # empty input?
+    if line == '':
+        continue
+
+    # about game
+    elif line in ('o hre', 'about', 'info'):
         print('Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou.')
         print('Túto hru spáchal v 2022 (c) mirek.')
 
+    # list of commands
     elif line in ('prikazy', 'commands', 'help', '?'):
         print('Zoznam dostupných príkazov:')
         print('* koniec - ukončí rozohratú hru')
         print('* o hre - zobrazí informácie o hre')
         print('* prikazy - zobrazí zoznam príkazov hry')
 
+    # quit game
     elif line in ('koniec', 'quit', 'q', 'bye'):
         line = input('Naozaj chceš skončiť? (a/n) ').lower().lstrip().rstrip()
         if line == 'a':
             game_state = states.QUIT
 
+    # unknown command
     else:
         print('Taký príkaz nepoznám.')
 
