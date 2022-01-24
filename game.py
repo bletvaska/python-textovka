cf8b0baa53f72eb8fffa4e1a61ab6dd56f3f9304
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-01-24 14:00:04 +0100

    pridanie aliasov

--- game.py
+++ game.py
@@ -5,22 +5,24 @@ print('Indiana Jones and his Great Escape')
 
 line = None
 
-while line != 'koniec':
+while line not in ('koniec', 'quit', 'q', 'bye'):
 
     line = input('> ').lower().lstrip().rstrip()
 
-    if line == 'o hre':
+    if line in ('o hre', 'about', 'info'):
         print('Ďalšie dobrodružstvo Indiana Jonesa. Tentkrát sa snaží ujsť z uzavretej kobky pod zemou.')
         print('Túto hru spáchal v 2022 (c) mirek.')
 
-    elif line == 'prikazy':
+    elif line in ('prikazy', 'commands', 'help', '?'):
         print('Zoznam dostupných príkazov:')
         print('* koniec - ukončí rozohratú hru')
         print('* o hre - zobrazí informácie o hre')
         print('* prikazy - zobrazí zoznam príkazov hry')
 
-    elif line == 'koniec':
-        pass
+    elif line in ('koniec', 'quit', 'q', 'bye'):
+        line = input('Naozaj chceš skončiť? (a/n) ').lower().lstrip().rstrip()
+        if line == 'a':
+            line = 'koniec'
 
     else:
         print('Taký príkaz nepoznám.')
