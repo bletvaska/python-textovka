633a639d84ddd7c46e76a441c0e90346ff15c313
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-01-28 09:44:47 +0100

    pridane miestnosti heaven a hell:

--- world.py
+++ world.py
@@ -19,7 +19,19 @@ world = [
         "name": "garden",
         "description": "Neudržiavaná plocha, ktorá sa pred pár rokmi určite nazývala záhradkou. Tuto boli asi zemiaky a tuto hľa asi aj krumpeľe.",
         "items": [],
-        "exits": {"north": None, "south": None, "east": None, "west": "dungeon"},
+        "exits": {"north": "heaven", "south": "hell", "east": None, "west": "dungeon"},
+    },
+    {
+        "name": "heaven",
+        "description": "Dvojnásobné RAMky. Dvojnásobné platy. Smiech a džavot naokolo. No to musí byť Dojč... Nebo.",
+        "items": [],
+        "exits": {"north": None, "south": "garden", "east": None, "west": None},
+    },
+    {
+        "name": "hell",
+        "description": "Ta teplo tu je. Začínam sa topiť vo vlastnej šťave. Ale aj v cudzej. Plač a škrípanie zubov. Python vo verzii 2.7.",
+        "items": [],
+        "exits": {"north": None, "south": None, "east": None, "west": None},
     },
 ]
 
