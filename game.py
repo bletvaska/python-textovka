5a7175e00dd42efb2c4fa42b1d44d981e59833e5
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-02-24 14:58:12 +0100

    vytvoreny modul helpers

--- game.py
+++ game.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 import states
+from helper import show_room, get_item_by_name
 from items import newspaper, door, bucket, canister, matches
 from items.features import MOVABLE
-
-
-def get_item_by_name(name: str, items: list):
-    for item in items:
-        if name == item.name:
-            return item
-
-    # return None
 
 
 def intro():
@@ -37,56 +30,6 @@ def outro():
     print('Created by (c) mirek - A very talented young python programmer.')
     print('Please support his next magic project by sending some funds')
     print('(at least 100 Euros). He will create something.')
-
-
-def show_room(room: dict) -> None:
-    """
-    Shows the content of the room
-
-    This function shows the content of the room: it's name, description, exits and items, which are located in the room.
-    @param room: the room to show
-    """
-    if not isinstance(room, dict):
-        # if type(room) != dict:
-        raise TypeError('Room is not of type dictionary.')
-
-    print(f'Nachádzaš sa v miestnosti {room["name"]}')
-    print(room["description"])
-
-    # show exits
-    translation = {
-        'north': 'sever',
-        'south': 'juh',
-        'east': 'východ',
-        'west': 'západ'
-    }
-
-    # check if there is any exit
-    some_exit = False
-    for ex in room['exits']:
-        if room['exits'][ex] is not None:
-            some_exit = True
-            break
-
-    if some_exit is False:
-        print('Z miestnosti nevedú žiadne východy.')
-    else:
-        print('Možné východy z miestnosti:')
-        for exit in room['exits']:
-            if room['exits'][exit] is not None:
-                print(f' * {translation[exit]}')
-
-    # print items
-    if len(room['items']) == 0:
-        print('Nevidíš tu nič zvláštne.')
-    else:
-        print('Vidíš:')
-        for item in room['items']:
-            print(f' * {item.name}')
-
-        # print('Vidíš:', ', '.join(room['items']))
-
-    # return None
 
 
 def main():
