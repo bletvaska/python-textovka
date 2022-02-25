62c3bc8db59a674d1ff72117c7db87db5fff2f7f
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-02-25 09:30:31 +0100

    tvorba novych prikazov pomocou dataclassov

---
+++ commands/inventory.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from .command import Command
+
+
+@dataclass
+class Inventory(Command):
+    name: str = 'inventar'
+    # aliases: list
+    description: str = 'zobrazí obsah hráčovho batoha'
+
+    def exec(self, backpack: list):
+        if len(backpack) == 0:
+            print('Batoh je prázdny.')
+        else:
+            print('V batohu máš:')
+            for item in backpack:
+                print(f' * {item.name}')
---
+++ commands/look_around.py
@@ -0,0 +1,18 @@
+# systemove importy/moduly
+from dataclasses import dataclass
+
+# tretostranove importy/moduly
+
+# moje vlastne importy/moduly
+from helper import show_room
+from .command import Command
+
+
+@dataclass
+class LookAround(Command):
+    name: str = 'rozhliadni sa'
+    # aliases: list
+    description: str = 'zobrazí opis miestnosti'
+
+    def exec(self, room: dict):
+        show_room(room)
