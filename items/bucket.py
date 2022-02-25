d31a8520611590eae8b20607eb84270cb8ff2f22
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-02-25 15:30:16 +0100

    pridane scenare pre pouzitie kybla aj zapaliek

--- items/bucket.py
+++ items/bucket.py
@@ -1,3 +1,4 @@
+from context import Context
 from .features import USABLE, MOVABLE
 
 name = 'vedro'
@@ -8,3 +9,21 @@ features = [
     USABLE,
     MOVABLE
 ]
+
+
+def use(context: Context):
+    # 1. preconditions
+    # som v miestnosti s dverami?
+    # horia dvere?
+
+    # 2. action
+    # aktualizujem vedro
+    # - nebude USABLE
+    # - aktualizujem opis (prazdne vedro)
+    # aktualizujem dvere
+    # - odstranim dvere z hry (zmazem ich z room['items']
+    # otvorim prechod do novej miestnosti
+    # - na zapad -> garden
+
+    # 3. render
+    print('masaker. ta si zahasil poziar a sa dvere rozpadli.')
--- items/matches.py
+++ items/matches.py
@@ -1,3 +1,4 @@
+from context import Context
 from .features import USABLE, MOVABLE
 
 name = 'zapalky'
@@ -9,3 +10,22 @@ features = [
     USABLE,
     MOVABLE
 ]
+
+
+def use(context: Context):
+    # 1. preconditions
+    # som v miestnosti s dverami?
+    # - zahrkal si zapalkami pri usku a presvedcil si sa, ze v nej naozaj nieco je.
+    # su dvere su poliate benzinom?
+    # *
+
+    # 2. action
+    # dvere zacnu horiet
+    # - aktualizujeme popis aj nazov dveri
+    # zapalky uz nebudu pouzitelne
+    # - odstranime USABLE zo zapaliek
+    # - aktualizujeme opis (prazdna krabicka)
+
+    # 3. render
+    print('Skrtol si zaplakou a dvere okamzite zblkli. Ak si si doteraz myslel, ze ti tu bola zima a este aj sero, '
+          'tak uz to tak nie je. Je tu prijemne teplucko.')
