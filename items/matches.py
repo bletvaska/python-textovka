321223e4a4f3c57ca485c8e45d8949ec2712b28a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-03-16 14:27:51 +0100

    pouzitie zapaliek

--- items/matches.py
+++ items/matches.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from context import Context
+from utils import get_item_by_name
 from .features import USABLE, MOVABLE
 from .item import Item
 
@@ -12,5 +14,33 @@ class Matches(Item):
     def __post_init__(self):
         self.features += [MOVABLE, USABLE]
 
+    def use(self, context: Context):
+        # init
+        room = context.room
+        backpack = context.backpack['items']
 
+        # 1. overim, ci su dvere poliate benzinom
+        #    - ci su dvere v stave 'wet'
+        door = get_item_by_name('dvere', room['items'])
+        if door.state != 'wet':
+            print('Zahrkal si krabičkou pri ušku, aby si sa uistil, že nie je prázdna. Usmial si sa.')
+            return
 
+        # 2. aktualizujem zapalky
+        #    - odstranim ich z hry
+        if self in room['items']:
+            room['items'].remove(self)
+        else:
+            backpack.remove(self)
+
+        # 3. aktualizujem dvere
+        #    - zmenim description - dvere v plamenoch
+        #    - zmenim stav na 'on fire'
+        door.description = 'Dvere v plameňoch.'
+        door.state = 'on fire'
+        door.name = 'horiace dvere'
+
+        # 4. renderujem scenu - skrtol si zapalkou a dvere zacali horiet
+        print('Otvoril si krabičku a vytiahol si z nej jedinú zápalku. Nadýchol si sa a škrtol si. Miestnosť sa '
+              'okamžite rozžiarila. Síce nie tvojim šarmantným úsmevom, ale plamenňom, ktorý oakmžite zachvátil dvere '
+              'nasiaknuté vysokooktánovým benzínom. Nejedno srdce pyromana by teraz vzplanulo radosťou.')
