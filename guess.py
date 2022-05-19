ff97e61ba833484eb0489fcfb3452aa3be64f73a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-19 14:37:04 +0200

    hra hotova

--- guess.py
+++ guess.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 
+import random
+
+def play_game(secret=13):
+    print('Myslím si číslo od 1 do 20.')
+
+    tip = None
+    count = 0
+    while tip != secret and count < 5:
+        tip = int(input('Tvoj tip: '))
+        count += 1
+
+        if tip > secret:
+            print(f'Hmm... Moje číslo je menšie ako {tip}.')
+        elif tip < secret:
+            print(f'Hmm... Moje číslo je väčšie ako {tip}.')
+        else:
+            print('Ta ty si genius.')
+            break
+    else:
+        print(f'Ta ty si jaká lama. Moje číslo bolo {secret}.')
+
+
 if __name__ == '__main__':
     playing = True
 
     while playing:
-        secret = 13
-        print('Myslím si číslo od 1 do 20.')
-
-        tip = None
-        count = 0
-        while tip != secret and count < 5:
-            tip = int(input('Tvoj tip: '))
-            count += 1
-
-            if tip > secret:
-                print(f'Hmm... Moje číslo je menšie ako {tip}.')
-            elif tip < secret:
-                print(f'Hmm... Moje číslo je väčšie ako {tip}.')
-            else:
-                print('Ta ty si genius.')
-                break
-        else:
-            print(f'Ta ty si jaká lama. Moje číslo bolo {secret}.')
+        secret = random.randint(1, 20)
+        play_game(secret)
 
         choice = input('Chceš si zahrať znova? (a/n) ').lower().strip()
         if choice not in ('a', 'y', 'ano', 'yes'):
