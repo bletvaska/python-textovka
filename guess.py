4f802d9b3137e0170a6de4c775ab7e5124a5a085
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-19 13:56:36 +0200

    5 pokusov na uhadnutie

--- guess.py
+++ guess.py
@@ -5,12 +5,20 @@ if __name__ == '__main__':
     print('Myslím si číslo od 1 do 20.')
 
     tip = None
-    while tip != secret:
+    count = 0
+    while tip != secret and count < 5:
         tip = int(input('Tvoj tip: '))
+        count += 1
 
         if tip > secret:
             print(f'Hmm... Moje číslo je menšie ako {tip}.')
         elif tip < secret:
             print(f'Hmm... Moje číslo je väčšie ako {tip}.')
+        else:
+            print('Ta ty si genius.')
+            break
+    else:
+    # if tip != secret:
+        print('Ta ty si jaká lama.')
 
-    print('Ta ty si genius.')
+    print('(c)2022 by mirek')
