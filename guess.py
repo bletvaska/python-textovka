dc78d269b300bc3c1e6c5ad8bbdb39229706343a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-19 13:27:45 +0200

    hadaj, pokial neuhadnes

--- guess.py
+++ guess.py
@@ -4,12 +4,13 @@ if __name__ == '__main__':
     secret = 13
     print('Myslím si číslo od 1 do 20.')
 
-    tip = int(input('Tvoj tip: '))
+    tip = None
+    while tip != secret:
+        tip = int(input('Tvoj tip: '))
 
-    if tip > secret:
-        print(f'Hmm... Moje číslo je menšie ako {tip}.')
-    elif tip < secret:
-        print(f'Hmm... Moje číslo je väčšie ako {tip}.')
-    # elif secret == tip:
-    else:
-        print('Ta ty si genius.')
+        if tip > secret:
+            print(f'Hmm... Moje číslo je menšie ako {tip}.')
+        elif tip < secret:
+            print(f'Hmm... Moje číslo je väčšie ako {tip}.')
+
+    print('Ta ty si genius.')
