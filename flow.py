47be3e771976d64d5dab08319578457ef7e80595
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-05-20 09:41:06 +0200

    stiahnutie dat o pocasi

--- flow.py
+++ flow.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
+import requests
+
+appid = '08f5d8fd385c443eeff6608c643e0bc5'
+query = 'kosice,sk'
+url = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q={}&appid={}'
+
 
 def main():
-    pass
+    scrape_data()
+    process_data()
+    export_data()
 
 
 def scrape_data():
-    pass
+    response = requests.get(url.format(query, appid))
+    print(response.status_code)
 
 
 def process_data():
