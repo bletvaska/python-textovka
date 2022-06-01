bebde1d36dd034a21f3e179cc0873a31e0281e1f
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-01 11:22:38 +0200

    game loop was created

--- game.py
+++ game.py
@@ -7,3 +7,9 @@ print(' | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\\__ \\')
 print('|___|_| |_|\\__,_|_|\\__,_|_| |_|\\__,_|  \\___/ \\___/|_| |_|\\___||___/')
 
 print('                         and his Great Escape')
+
+line = None
+while line != 'koniec':
+    line = input('> ')
+
+print('Dobru chut.')
