b95a2c9286f395409e3cfbe70be0d2c35aef00d9
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-01 13:30:30 +0200

    command about was added

--- game.py
+++ game.py
@@ -12,4 +12,9 @@ line = None
 while line != 'koniec':
     line = input('> ').lower().lstrip().rstrip()
 
+    if line == 'o hre':
+        print('(c)2022 created by mighty mire(c) the programmer')
+        print('Ďalšie dobrodružstvo Indiana Jonesa. Tentokrát je jeho úlohou uniknúť z podzmeného väzenia, '
+              'v ktorom sa náhodou ocitol.')
+
 print('Dobru chut.')
