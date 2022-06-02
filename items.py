9608c720cedc18bf1a84c5b6b466c6621e14364d
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-02 13:09:06 +0200

    refaktoring of items

--- items.py
+++ items.py
@@ -5,3 +5,21 @@ from dataclasses import dataclass
 class Item:
     name: str
     description: str
+
+
+@dataclass
+class Whip:
+    name: str = 'bic'
+    description: str = 'Tvoj neoceniteľný kamarát na každom jednom dobrodužstve.'
+
+
+@dataclass
+class Newspaper:
+    name: str = 'noviny'
+    description: str = 'Posledné vydanie Bravíčka. To najlepšie čítanie pre každého chovateľa Pytóna.'
+
+
+@dataclass
+class Revolver:
+    name: str = 'revolver'
+    description: str = 'Štandardný revolver značky Smis-end-Weson'
