fa3b54fdc564a032c7d99ec433d03dc6824d8460
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-02 13:41:26 +0200

    usage of newspaper

--- items.py
+++ items.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import random
 
 
 @dataclass
@@ -17,6 +18,18 @@ class Whip:
 class Newspaper:
     name: str = 'noviny'
     description: str = 'Posledné vydanie Bravíčka. To najlepšie čítanie pre každého chovateľa Pytóna.'
+
+    def use(self):
+        print('Aaaa Bravíčko. Zalistoval si čerstvým vydaním tohto legendárneho časopisu každého správneho Pytonistu '
+              'a píšu...')
+
+        headlines = [
+            'Python 3.11 bude vydany v piatok 13.',
+            'Spravny Pythonista pouziva len Python >= 3.7',
+            'import this'
+        ]
+
+        print(random.choice(headlines))
 
 
 @dataclass
