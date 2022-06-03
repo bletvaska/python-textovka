e847d928696b6a2c39126f7c8f1c4e589d4b4478
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-03 13:35:21 +0200

    refactoring context

--- context.py
+++ context.py
@@ -1,11 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
+import states
+from commands.command import Command
 from items.item import Item
 
 
 @dataclass
 class Context:
-    commands: List[Item]
-    backpack: List[Item]
-    game_state: str
+    commands: List[Command]
+    backpack: List[Item] = field(default_factory=lambda: [])
+    game_state: str = states.PLAYING
