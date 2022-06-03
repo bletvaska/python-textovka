5136663941566ea1456ac72db26bbb9eb3a3807f
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-03 09:23:57 +0200

    added features to items

--- items.py
+++ items.py
@@ -1,23 +1,30 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import random
+
+MOVABLE = 1
+USABLE = 2
+EXPLORABLE = 3
 
 
 @dataclass
 class Item:
     name: str
     description: str
+    features: list[int]
 
 
 @dataclass
 class Whip:
     name: str = 'bic'
     description: str = 'Tvoj neoceniteľný kamarát na každom jednom dobrodužstve.'
+    features: list[int] = field(default_factory=lambda: [MOVABLE])
 
 
 @dataclass
 class Newspaper:
     name: str = 'noviny'
     description: str = 'Posledné vydanie Bravíčka. To najlepšie čítanie pre každého chovateľa Pytóna.'
+    features: list[int] = field(default_factory=lambda: [MOVABLE, USABLE])
 
     def use(self):
         print('Aaaa Bravíčko. Zalistoval si čerstvým vydaním tohto legendárneho časopisu každého správneho Pytonistu '
@@ -36,3 +43,4 @@ class Newspaper:
 class Revolver:
     name: str = 'revolver'
     description: str = 'Štandardný revolver značky Smis-end-Weson'
+    features: list[int] = field(default_factory=lambda: [MOVABLE])
