47b773987692e11023a75df2f326c0e653648216
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-06 10:31:32 +0200

    refaktoring matches

--- items/matches.py
+++ items/matches.py
@@ -13,4 +13,23 @@ class Matches(Item):
     features: List[int] = field(default_factory=lambda: [MOVABLE, USABLE])
 
     def use(self, context: Context):
+        # arrange
+        # 1. v batohu/miestnosti sa nachadzaju dvere poliate benzinom. a ak nie, tak napiseme:
+        #
+        #     Zahrkal si krabickou od zapaliek, aby si sa uistil, ze v nej nieco je. A fakt - su v nej tri zapalky.
+        #
+        # # act
+        # 2. aktualizujeme zapalky
+        #     odstranime USABLE zo zoznamu ficur
+        #     aktualizujeme opis: Prázdna krabička bezpečnostných zápaliek značky BILLA.
+        #
+        # 3. aktualizujeme dvere
+        #     aktualizujeme opis: Masivne veľké dubové horiace dvere.
+        #     aktualizujeme nazov: horiace dvere
+        #
+        # # render
+        # 4. vypiseme na obrazovku
+        #
+        # Skrtol si zapalkou a dvere okamzite zachvatili plamene. Obrovske teplo ta donutilo urobit krok vzad, cim sa ti naskytol vyrazne lepsi pohlad na celu tuto milu horiacu scenu.
+
         print('>> pouzitie zapaliek')
