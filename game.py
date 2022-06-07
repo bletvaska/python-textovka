39cd40af51835bc38189c3a3f754baf39cfd55dd
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-06-07 09:46:48 +0200

    created command drop item

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 import states
 from commands.about import About
 from commands.commands import Commands
+from commands.drop import Drop
 from commands.examine import Examine
 from commands.inventory import Inventory
 from commands.lookaround import LookAround
@@ -40,6 +41,7 @@ context = Context(
 context.commands = [
     About(),
     Commands(),
+    Drop(),
     Examine(),
     Inventory(),
     LookAround(),
