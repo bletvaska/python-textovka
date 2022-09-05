3e14b25daea589a89cc89627ace8c57c4d479769
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-05 14:10:13 +0200

    normalize input string

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
 
     line = None
     while line != 'koniec':
-        line = input('> ')
+        # normalize input string
+        line = input('> ').lower().lstrip().rstrip()
