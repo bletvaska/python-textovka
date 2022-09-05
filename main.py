f8696bbcf07a92d97645ba596c79e3ce453676f7
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-05 14:56:38 +0200

    if-elif

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ if __name__ == '__main__':
         # normalize input string
         line = input('> ').lower().lstrip().rstrip()
 
-        if line == 'o hre':
+        if line == '':  # len(line) == 0
+            # pass
+            continue
+
+        elif line == 'o hre':
             print('Túto megašupabombašpica hru vytvoril v (c)2022 mladý nádejný a atraktívny programátor mirek')
             print('Hra je ďaľším pokračovaním nestarnúceho dobrodruha Indiana Jonesa. Tentokrát je jeho úlohou dostať '
                   'sa zo zajatia fašistickej ponorky.')
+
+        elif line == 'prikazy':
+            print('V hre je možné použiť tieto príkazy:')
+            print('* koniec - ukončí rozohratú hru')
+            print('* o hre - zobrazí informácie o hre')
+            print('* prikazy - zobrazí zoznam dostupných príkazov v hre')
+
+        elif line == 'pomoc':
+            print('Ta pomôž si sám.')
