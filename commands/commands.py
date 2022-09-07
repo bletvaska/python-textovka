c7d07af06b49419b942e105b6e6563b9766b9fe2
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-07 14:35:10 +0200

    added new command Commands

---
+++ commands/commands.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from .command import Command
+
+
+@dataclass
+class Commands(Command):
+    name: str = 'prikazy'
+    description: str = 'zobrazí zoznam dostupných príkazov v hre'
+
+    def exec(self):
+        print('V hre je možné použiť tieto príkazy:')
+        print('* inventar - zobrazí obsah batohu')
+        print('* koniec - ukončí rozohratú hru')
+        print('* o hre - zobrazí informácie o hre')
+        print('* pouzi - použije zvolený predmet')
+        print('* preskumaj - preskúma zvolený predmet')
+        print('* prikazy - zobrazí zoznam dostupných príkazov v hre')
