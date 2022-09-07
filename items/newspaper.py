ad34383065857b66724354571ebca18a132fe5aa
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-07 13:50:46 +0200

    created usage of newspaper

--- items/newspaper.py
+++ items/newspaper.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass, field
 
 from .features import USABLE, MOVABLE
@@ -9,8 +10,15 @@ class Newspaper(Item):
     name: str = 'noviny'
     description: str = 'Čerstvé vydanie Denníka N.'
     features: list[int] = field(default_factory=lambda: [MOVABLE, USABLE])
+    headlines: list[str] = field(default_factory=lambda: [
+        'V gigantických katakombách bojovali partizáni proti nacistom, teraz chránia Odesu pred Rusmi (reportáž z podzemia)',
+        'Iveta Radičová: Premiér Heger mal konať (+ video)',
+        'Rus, ktorý zaskočil slovenských konšpirátorov: Nečakal som, že je u vás Putin taký obľúbený',
+        'Košická nemocnica uplatňuje pri výbere primárov zvláštne pravidlá, vďaka nim uspel Paškov známy',
+        'Týždeň v zdraví: Viete, že fajčenie vážne škodí aj srdcu? Je všetka vegánska strava naozaj zdravá? Prečo nás po plači bolí hlava?'
+    ])
 
     def use(self):
         print('Ta zalistoval som si v denníku a čítam, že:')
-        print('V gigantických katakombách bojovali partizáni proti nacistom, teraz chránia Odesu pred Rusmi (reportáž '
-              'z podzemia)')
+        headline = random.choice(self.headlines)
+        print(headline)
