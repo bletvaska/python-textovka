9366cd52131134983a54a10ad6aba1db60d147b7
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-08 10:38:25 +0200

    created Room dataclass

---
+++ rooms/room.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+from items.item import Item
+
+
+@dataclass
+class Room:
+    name: str
+    description: str
+    items: list[Item] = field(default_factory=list)  # list[Item] = []
+    # exits: list[]
+    # north: str
+    # south: str
+    # east: str
+    # west: str
+    # up: str
+    # down: str
