5fe345cfb9d5264f41f292b7a880aec574e3e59b
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-08 14:57:11 +0200

    update for method .show() for exits from the room

--- rooms/room.py
+++ rooms/room.py
@@ -31,3 +31,26 @@ class Room:
                 print(f'* {item.name}')
 
         print()
+
+        # exits from room
+        # if self.south is None && self.north is None && ...
+        if any([self.north, self.south, self.east, self.west, self.up, self.down]):
+            print('Možné východy z miestnosti:')
+
+            if self.north is not None:
+                print('* sever')
+            if self.south is not None:
+                print('* juh')
+            if self.east is not None:
+                print('* východ')
+            if self.west is not None:
+                print('* západ')
+            if self.down is not None:
+                print('* dolu')
+            if self.up is not None:
+                print('* hore')
+
+        else:
+            print('Z tejto miestnosti nevedú žiadne východy.')
+
+        print()
