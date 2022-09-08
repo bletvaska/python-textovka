8b76c8e1c0aaa5bdfd55425d3c0f7a3b06134e5c
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-08 14:02:07 +0200

    refactoring of take command - defense programming example

--- commands/take.py
+++ commands/take.py
@@ -11,22 +11,30 @@ class Take(Command):
     description: str = 'vezme predmet z miestnosti a vloží ho do batohu'
 
     def exec(self, context):
+        # is there a name of item given?
         if self.parameter == '':
             print('Neviem čo chceš vziať.')
-        else:
-            item = get_item_by_name(self.parameter, context.current_room.items)
+            return
 
-            if item is None:
-                print('Taký predmet tu nikde nevidím.')
-            else:
-                if MOVABLE not in item.features:
-                    print('Tento predmet sa nedá zobrať.')
-                else:
-                    # remove item from room
-                    context.current_room.items.remove(item)
+            # search for item by name
+        item = get_item_by_name(self.parameter, context.current_room.items)
 
-                    # insert item to backpack
-                    context.backpack.append(item)
+        # wat item found?
+        if item is None:
+            print('Taký predmet tu nikde nevidím.')
+            return
 
-                    # render
-                    print(f'Do batohu si vložil predmet {item.name}.')
+        # is item movable?
+        if MOVABLE not in item.features:
+            print('Tento predmet sa nedá zobrať.')
+            return
+
+        # action
+        # remove item from room
+        context.current_room.items.remove(item)
+
+        # insert item to backpack
+        context.backpack.append(item)
+
+        # render
+        print(f'Do batohu si vložil predmet {item.name}.')
