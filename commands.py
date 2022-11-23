2db71b4455d6f4186722a54df0517cc2eb2005ba
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-23 10:27:23 +0100

    created class commands

--- commands.py
+++ commands.py
@@ -20,6 +20,21 @@ class About(Command):
     def exec(self):
         print('Hru Indiana Jones 2 napísal mladý nádejný programátor v jazyku Python - mirek v roku 2022.')
 
+
+@dataclass
+class Commands(Command):
+    # fields
+    name: str = 'prikazy'
+    description: str = 'zobrazí dostupné príkazy v hred'
+
+    # methods
+    def exec(self):
+        print('V hre je možné použiť tieto príkazy:')
+        print('* inventar - zobrazi obsah hráčovho batohu')
+        print('* koniec - ukončí hru')
+        print('* o hre - zobrazi informacie o hre')
+        print('* prikazy - zoznam dostupných príkazov v hre')
+
 # * prikazy
 #   * opis
 #   * nazov
