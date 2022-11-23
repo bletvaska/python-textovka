7f47f60417d1e3efe87d7d1f3e05491e9e539bd9
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-23 11:31:19 +0100

    great banner

--- commands/command.py
+++ commands/command.py
@@ -12,4 +12,5 @@ class Command:
 
     # behavior / methods
     def exec(self):
+        #! FIXME exception
         print('vykonavam prikaz')
--- helpers.py
+++ helpers.py
@@ -2,10 +2,15 @@ def intro():
     """
     Prints the intro banner of the game.
 
-    Simple function which shows predefinet mocny banner on the screen.
+    Simple function which shows mocny banner on the screen.
     The banner is a representation of ascii art.
     """
-    print('Indiana Jones 2')
+    print(" ___           _ _                         _                         ____  ")
+    print("|_ _|_ __   __| (_) __ _ _ __   __ _      | | ___  _ __   ___  ___  |___ \\ ")
+    print(" | || '_ \\ / _` | |/ _` | '_ \\ / _` |  _  | |/ _ \\| '_ \\ / _ \\/ __|   __) |")
+    print(" | || | | | (_| | | (_| | | | | (_| | | |_| | (_) | | | |  __/\\__ \\  / __/ ")
+    print("|___|_| |_|\\__,_|_|\\__,_|_| |_|\\__,_|  \\___/ \\___/|_| |_|\\___||___/ |_____|")
+    print('and his Great Python Adventure'.center(80))
 
 
 def outro():
