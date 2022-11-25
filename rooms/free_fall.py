880394af4b2377961a20a3e50ca5c796773636cc
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-25 14:41:28 +0100

    refaktoring

--- rooms/free_fall.py
+++ rooms/free_fall.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 
-from helpers import get_item_by_name
-from states import SHOT_BY_ENEMY, DEATH_BY_FREE_FALL
+from states import DEATH_BY_FREE_FALL
 from .room import Room
 
 
 @dataclass
 class FreeFall(Room):
     def on_enter(self, context):
-        print('Ta si spadol a zabil sa bez padaka.')
+        print('Stal si sa zakladateľom športového odvetvia, ktoré vojde do histórie ako skok hlboký.')
         context.game_state = DEATH_BY_FREE_FALL
