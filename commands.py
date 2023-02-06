dc89af116a3268a7b58d3238f21eac06dafecd24
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-02-06 14:32:17 +0100

    added class Commands()

--- commands.py
+++ commands.py
@@ -15,10 +15,23 @@ class Command(BaseModel):
 
 
 class About(Command):
-    # fields
+    """
+    Shows info about the game.
+    """
     name = 'o hre'
     description = 'zobrazí informácie o hre'
 
     def exec(self):
         print('(c)2023 created by mirek')
         print('Dalšie dobrodružstvo Indiana Jonesa je tentokrát vytvorené v jazyku Python.')
+
+
+class Commands(Command):
+    name = 'prikazy'
+    description = 'zobrazí zoznam dostupných príkazov v hre'
+
+    def exec(self):
+        print('V hre je možné použiť tieto príkazy:')
+        print('* koniec - ukončí rozohratú hru')
+        print('* o hre - zobrazí informácie o hre')
+        print('* prikazy - zobrazí zoznam dostupných príkazov v hre')
