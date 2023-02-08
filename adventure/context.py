5a0dcbc93c311a7c72e77e9cb6b2c566d9a34c5c
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-02-08 09:43:11 +0100

    fixed issue with circular imports

--- adventure/context.py
+++ adventure/context.py
@@ -1,13 +1,12 @@
 from pydantic import BaseModel
 
 import states
-from commands.command import Command
 from items.item import Item
 from rooms import Room
 
 
 class Context(BaseModel):
     current_room: Room
-    commands: list[Command]
+    commands: list
     game_state = states.PLAYING
     backpack: list[Item] = []
