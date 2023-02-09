026005ced15679640ffa108e1e0fc461cf61c4d8
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-02-09 14:44:10 +0100

    created coconut palm tree

---
+++ adventure/items/coconut_palm_tree.py
@@ -0,0 +1,17 @@
+from context import Context
+from .features import EXPLORABLE
+from .item import Item
+
+
+class CoconutPalmTree(Item):
+    name = 'kokosova palma'
+    description = 'Zdá sa, že na jej plody nedosiahneš.'
+    features = [EXPLORABLE]
+
+    def explore(self, context: Context):
+        # act/explore
+        context.current_room.items.append(NaziUniform())
+        self.features.remove(EXPLORABLE)
+
+        # render
+        print('Pod koreňmi palmy si objavil schovanú uniformu!')
