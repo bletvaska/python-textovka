b6b919fd5c77aaa7f500c8478cde70fec9bc9b7c
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-02-09 13:47:34 +0100

    usage of parachute, stage 1

--- adventure/items/parachute.py
+++ adventure/items/parachute.py
@@ -1,3 +1,4 @@
+from context import Context
 from .features import MOVABLE, USABLE
 from .item import Item
 
@@ -7,5 +8,15 @@ class Parachute(Item):
     description = 'Obyčajný padák MADE IN U.S.A. 1933'
     features = [MOVABLE, USABLE]
 
-    def use(self, context):
+    def use(self, context: Context):
+        # check usability
+        # musim byt v miesntosti volny pad
+        if context.current_room.name != 'voľný pád':
+            print('Podľa teba som zrejme blbec, ale naozaj nechápem, na čo by to v tejto chvíli bolo dobré.')
+            return
+
+        # act
+        # teleportujem sa do miestnosti miesto pristatia
+
+        # render
         print('pouzivam padak')
