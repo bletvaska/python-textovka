2c99901c02d57a3fa6b301d98ee2cbdf57b1543f
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-02-10 09:34:16 +0100

    exit opened to camp, when indy is wearing nazi uniform

--- adventure/rooms/at_enemy_gate.py
+++ adventure/rooms/at_enemy_gate.py
@@ -1,6 +1,16 @@
+import states
 from rooms import Room
+from rooms.directions import SOUTH
 
 
 class AtEnemyGate(Room):
     def act(self, context):
-        pass
+        for item in context.backpack:
+            if item.name == 'nemecka uniforma (oblecena)':
+                if SOUTH not in context.current_room.exits:
+                    context.current_room.exits[SOUTH] = 'v tábore'
+                    print('Keď ťa vojak uvidel, otvoril ti bránu (hlupák).')
+                return
+        else:
+            print('Vojak si ťa so záujmom prehliadol a zastrelil ťa...')
+            context.game_state = states.SHOT_BY_ENEMY
--- adventure/states.py
+++ adventure/states.py
@@ -11,3 +11,6 @@ PLANE_CRASH = 'plane crash'
 
 # indy died by not opening parachute
 DEATH_BY_FREE_FALL = 'death by free fall'
+
+# indy was shot without nazi uniform
+SHOT_BY_ENEMY = 'shot by enemy'
