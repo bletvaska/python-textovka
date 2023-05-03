a4bc88fb4018fe97a0b1914b1dbc5407fc6cecea
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-05-03 12:19:20 +0200

    first run

---
+++ game.py
@@ -0,0 +1,22 @@
+print("Indiana Jones and his Greatest Python Adventure")
+line = None
+
+while line != 'koniec':
+    line = input('> ').lower().lstrip().rstrip()
+
+    if line == 'o hre':
+        print('Túto mocnú hru spáchal mocný programátor mirek')
+        print('(c)2023 by mirek')
+
+    elif line == 'pre evku':
+        print('toto je vyrobene vylucne pre evku.')
+
+    elif line == 'prikazy':
+        print('Zoznam dostupnych prikazov v hre:')
+        print('* o hre - zobrazi informacie o hre')
+        print('* prikazy - zobrazi zoznam dostupnych prikazov')
+
+    else:
+        print('Taký príkaz nepoznám.')
+
+print('koniec')
