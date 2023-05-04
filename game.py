1b4a00118b99798af9146bac1bb4615bd4e10e7d
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-05-04 11:04:26 +0200

    first attempt for command parser

--- game.py
+++ game.py
@@ -3,25 +3,34 @@ from helpers import intro, outro
 from states import STATE_PLAYING, STATE_QUIT
 
 game_state = STATE_PLAYING
+commands = [
+    About(),
+    Commands(),
+    Quit(),
+]
 
 intro()
 
 while game_state == STATE_PLAYING:
     line = input('> ').lower().lstrip().rstrip()
 
-    if line == '':  # len(line) == 0
-        continue
+    for cmd in commands:
+        if cmd.name == line:
+            cmd.exec()
 
-    elif line == 'o hre':
-        About().exec()
-
-    elif line == 'prikazy':
-        Commands().exec()
-
-    elif line == 'koniec':
-        game_state = Quit().exec()
-
-    else:
-        print('Taký príkaz nepoznám.')
+    # if line == '':  # len(line) == 0
+    #     continue
+    #
+    # elif line == 'o hre':
+    #     About().exec()
+    #
+    # elif line == 'prikazy':
+    #     Commands().exec()
+    #
+    # elif line == 'koniec':
+    #     game_state = Quit().exec()
+    #
+    # else:
+    #     print('Taký príkaz nepoznám.')
 
 outro()
