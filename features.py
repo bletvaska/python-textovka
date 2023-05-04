e5cd56751d54a58c5ddaa2bff5c82a4362d4fbea
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-05-04 14:38:34 +0200

    created Whip and features

---
+++ features.py
@@ -0,0 +1,5 @@
+# when item can be moved between rooms
+MOVABLE = 1
+
+# when item can be used (somehow)
+USABLE = 2
--- items.py
+++ items.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 
+from features import USABLE, MOVABLE
+
 
 class Item(BaseModel):
     name: str
     description: str
     features: list = []
+
+
+class Whip(Item):
+    name = 'bic'
+    description = 'tvoj neoceniteľný pomocník...!'
+    features = [MOVABLE, USABLE]
