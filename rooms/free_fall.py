ee5d9feaadb2440cec2326d92e1b16c4d10ae878
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-05-12 09:58:33 +0200

    player dies in free fall

--- rooms/free_fall.py
+++ rooms/free_fall.py
@@ -10,10 +10,9 @@ class FreeFall(Room):
     description = 'Vznášaš sa medzi oblakmi. Uži si tento zaujímavý pocit a nevšímaj si zem, ktorá sa rapídne približuje. Mimochodom, v diaľke na [bold yellow]juhu[/bold yellow] je vidieť nejaký vojenský tábor.'
     counter = 0
 
-    # def act(self, context):
-    #     self.counter = self.counter + 1
-    #
-    #     if self.counter == 4:
-    #         print(
-    #             'Lietadla šťastne pristálo (strmhlav). Je mi to ľúto, ale ani taký profesionál, ako je Indiana Jones nemohol prežiť podobnú radostnú udalosť.')
-    #         context.game_state = states.STATE_DEATH
+    def act(self, context):
+        self.counter = self.counter + 1
+
+        if self.counter == 2:
+            print('Stal si sa zakladateľom športového odvetvia, ktoré vojde do histórie ako skok hlboký.')
+            context.game_state = states.STATE_DEATH
