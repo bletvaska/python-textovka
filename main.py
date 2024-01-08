6ff3e1991f602fa84b096dcf462d44d0f668eaa5
mirek <mirek@cnl.sk>
2024-01-08 14:24:18 +0100

    pridany modul so stavmi hry

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from states import STATE_PLAYING, STATE_QUIT
+
 print("Indiana Jones and his Greatest Python Adventure")
 
-line = None
-while line != 'koniec':
+
+game_state = STATE_PLAYING
+while game_state == STATE_PLAYING:
     line = input('> ').lower().lstrip().rstrip()   # echo line | lower | lstrip | rstrip
 
     if line == '':  # if len(line) == 0:
         pass  # {}
 
     elif line == 'o hre':
-        print('(c)2023 created by mirek')
+        print('(c)2024 created by mirek')
         print('Dalsie dobrodruzstvo indiana jonesa tentokrat vytvorene v jazyku Python.')
 
     elif line == 'prikazy':
@@ -19,6 +22,7 @@ while line != 'koniec':
 
     elif line == 'koniec':
         print('Ďakujem, že si si zahral túto úžasnú (ukradnutú) hru.')
+        game_state = STATE_QUIT
 
     else:
         print('Taký príkaz nepoznám.')
---
+++ states.py
@@ -0,0 +1,5 @@
+# main state, when game is playing
+STATE_PLAYING = 'playing'
+
+# when QUIT command was entered
+STATE_QUIT = 'quit'
