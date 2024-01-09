4ca5ea28c8f7df5b0405d6ab380b0fbdf95437ea
mirek <mirek@cnl.sk>
2024-01-09 14:30:09 +0100

    refaktoring - osetrenie neexistujuceho prikazu  pomocou if-else, ale aj pomocou vynimky

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import states
-from commands import About, Commands, Quit
+from commands import About, Commands, Quit, Command
 from helpers import intro, outro
+
+
+def parse_line(line: str, commands: list[Command]):
+    for command in commands:
+        if line == command.name:
+            return command
+
+    return None
 
 
 # game initialization
@@ -17,22 +25,21 @@ intro()
 while game_state == states.PLAYING:
     line = input('> ').lower().lstrip().rstrip()  # echo line | lower | lstrip | rstrip
 
+    # is line empty?
     if line == '':  # if len(line) == 0:
-        pass  # {}
+        continue  # {}
 
-    elif line == 'o hre':
-        cmd = About()
-        cmd.exec()
-
-    elif line == 'prikazy':
-        cmd = Commands()
-        cmd.exec()
-
-    elif line == 'koniec':
-        cmd = Quit()
-        cmd.exec()
-
-    else:
+    # parse and execute command
+    command = parse_line(line, commands)
+    if command is None:
         print('Taký príkaz nepoznám.')
+    else:
+        command.exec()
+
+    # try:
+    #     command = parse_line(line, commands)
+    #     command.exec()
+    # except AttributeError:
+    #     print('Taký príkaz nepoznám.')
 
 outro()
