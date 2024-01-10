252ed6e309e465c84d2a798cd004da903bd3555b
mirek <mirek@cnl.sk>
2024-01-10 10:51:54 +0100

    vytvorena trieda Item a zoznam vlastnosti pre predmety

---
+++ items/features.py
@@ -0,0 +1,8 @@
+# if the item can be taken/dropped to/from backpack
+MOVABLE = 1
+
+# if it is possible to use the item
+USABLE = 2
+
+# if it is possible to make some action after item is examined
+EXAMINABLE = 3
---
+++ items/item.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+    features: list[int] = []
+
