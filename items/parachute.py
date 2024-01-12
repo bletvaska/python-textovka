4f8bbd1e63aa069ad68b69432b3e37182c69c4b6
mirek <mirek@cnl.sk>
2024-01-12 13:00:35 +0100

    padak je pouzitelny

--- items/parachute.py
+++ items/parachute.py
@@ -1,3 +1,7 @@
+from rich import print
+
+from game_context import GameContext
+from helpers import get_room_by_name
 from items.features import USABLE, MOVABLE
 from items.item import Item
 
@@ -6,3 +10,16 @@ class Parachute(Item):
     name: str = 'padak'
     description: str = 'obyčajný padák. Made in U.S.A. 1933'
     features: list[int] = [MOVABLE, USABLE]
+
+    def use(self, context: GameContext):
+        # sme v spravnej miestnosti?
+        if context.current_room.name != 'vo vzduchu':
+            print('Podľa teba som zrejme blbec, ale naozaj nechápem, na čo by to v tejto chvíli bolo dobré.')
+            return
+
+        # presun sa do miestnosti pust
+        context.current_room = get_room_by_name('púšť', context.world)
+
+        # render
+        print('[bold green]Nad hlavou sa ti roztvoril padák a po chvíli si šťastne pristál...[/bold green]')
+        context.current_room.show()
