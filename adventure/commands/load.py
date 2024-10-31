fc1c6cdb1efef60f68e4d2cd624690c69bcc02ac
mirek <mirek@cnl.sk>
2024-10-31 13:54:51 +0100

    ukazka nacitania ulozenej pozicie

--- adventure/commands/load.py
+++ adventure/commands/load.py
@@ -5,17 +5,25 @@ from .command import Command
 
 class Load(Command):
     name: str = 'nahraj'
-    description: str = 'nahrá uloženú pozíciu hry zo súboru'
+    description: str = 'načíta uloženú pozíciu hry zo súboru'
 
     def exec(self, context, filename):
         if filename == '':
-            print('Neviem, z akého súboru chceš nahrať svoju uloženú pozíciu.')
+            print('Neviem, z akého súboru chceš načítať svoju uloženú pozíciu.')
             return
 
         with open(filename, 'r') as file:
             history = json.load(file)
 
-        print('Tvoja pozícia bola úspešne nahraná.')
+        # new_context = GameContext()
+        # new_context.current_room = get_room_by_name('lietadlo', context.world)
+        #
+        # for line in history:
+        #     cmd, param = parse_line(line, new_context.commands)
+        #     cmd.exec(new_context, param)
 
         for line in history:
             print(line)
+
+        print('Tvoja pozícia bola úspešne načítaná.')
+
