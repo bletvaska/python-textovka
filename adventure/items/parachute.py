7caf14623b00e8cf73db2f48390034825c131eb2
mirek <mirek@cnl.sk>
2024-10-31 11:39:20 +0100

    pouzitie padaka

--- adventure/items/parachute.py
+++ adventure/items/parachute.py
@@ -1,3 +1,4 @@
+from helpers import get_room_by_name
 from items.features import MOVABLE, USABLE
 from items.item import Item
 
@@ -8,5 +9,13 @@ class Parachute(Item):
     features: list[int] = [MOVABLE, USABLE]
 
     def use(self, context) -> bool:
+        # overit, ci sa predmet da pouzit
+        if context.current_room.name != 'vo vzduchu':
+            return False
+
+        # pouzitie predmetu
+        context.current_room = get_room_by_name('púšť', context.world)
         print('Nad hlavou sa ti roztvoril padák a po chvíli si šťastne pristál...')
+        print()
+        context.current_room.show()
         return True
