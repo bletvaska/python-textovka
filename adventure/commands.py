f6e6a6b4fb98298f42850e4dd9e86a66046185f6
mirek <mirek@cnl.sk>
2024-11-21 13:07:40 +0100

    type hint pre Commands.exec()

--- adventure/commands.py
+++ adventure/commands.py
@@ -38,7 +38,7 @@ class Commands(Command):
     name: str = "prikazy"
     description: str = "zobrazí zoznam dostupných príkazov v hre"
 
-    def exec(self, commands):
+    def exec(self, commands: list[Command]):
         print('V hre je možné použiť tieto príkazy:')
         for command in commands:
             print(f'* [bold cyan]{command.name}[/bold cyan] - {command.description}')
