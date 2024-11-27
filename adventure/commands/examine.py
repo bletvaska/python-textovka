465de105133e3d92ada55f093771467fbd2cb10b
mirek <mirek@cnl.sk>
2024-11-27 14:47:48 +0100

    prikaz preskumaj

--- adventure/commands/examine.py
+++ adventure/commands/examine.py
@@ -6,4 +6,16 @@ class Examine(Command):
     description: str = 'zobrazí informácie o zvolenom predmete'
 
     def exec(self, context):
-        print(f'skumam predmet {self.param}')
+        # if not item was entered
+        if self.param == '':
+            print('Neviem, čo chceš preskúmať.')
+            return
+
+        # search for item and print description if found
+        for item in context.current_room.items:
+            if item.name == self.param:
+                print(item.description)
+                return
+
+        # not found
+        print('Taký predmet tu nikde nevidím.')
