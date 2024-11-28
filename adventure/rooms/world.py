59b283488b800a7e896c35655709d77f8ae01a0e
mirek <mirek@cnl.sk>
2024-11-28 13:45:40 +0100

    refaktoring dokumentacie

--- adventure/rooms/world.py
+++ adventure/rooms/world.py
@@ -1,7 +1,6 @@
 from items.empty_seats import EmptySeats
 from items.whip import Whip
 from .directions import NORTH, SOUTH, EAST, WEST, UP, DOWN
-# from .free_fall import FreeFall
 from .plane import Plane
 from .room import Room
 
