080447bb940d6c1764fa7e8fc10949bbe095a96b
mirek <mirek@cnl.sk>
2024-11-28 09:23:37 +0100

    osetrenie prazdneho prikazu vezmi

--- adventure/commands/take.py
+++ adventure/commands/take.py
@@ -6,4 +6,9 @@ class Take(Command):
     description: str = 'vezme predmet z miestnosti a vloží ho do batohu'
 
     def exec(self, context):
+        # if not item was entered
+        if self.param == '':
+            print('Neviem, čo chceš zobrať.')
+            return
+
         print(f'pokusam sa zobrat predmet {self.param}.')
