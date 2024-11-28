277027a914800839128e5f4f2f5e1517f1693853
mirek <mirek@cnl.sk>
2024-11-28 10:04:31 +0100

    funkcia get_item_by_name()

--- adventure/helpers.py
+++ adventure/helpers.py
@@ -1,4 +1,5 @@
 from commands.command import Command
+from items.item import Item
 
 
 def intro():
@@ -27,5 +28,13 @@ def parse_line(line: str, commands: list[Command]) -> Command | None:
         if line.startswith(command.name):
             command.param = line.split(command.name)[1].lstrip()
             return command
+
+    # return None
+
+
+def get_item_by_name(name: str, items: list[Item]) -> Item | None:
+    for item in items:
+        if item.name == name:
+            return item
 
     # return None
